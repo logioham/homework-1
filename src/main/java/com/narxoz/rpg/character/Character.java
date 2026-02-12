ecc08471108bc4c94c6ab94eefb5c95ac978dcde
logioham <dias.akzhigit@narxoz.kz>
2026-02-12 23:26:56 +0500

    character class

--- src/main/java/com/narxoz/rpg/character/Character.java
+++ src/main/java/com/narxoz/rpg/character/Character.java
@@ -1,5 +1,8 @@
 package com.narxoz.rpg.character;
 
+import com.narxoz.rpg.equipment.Armor;
+import com.narxoz.rpg.equipment.Weapon;
+
 /**
  * Base interface for all character types in the RPG system.
  *
@@ -12,19 +15,16 @@ package com.narxoz.rpg.character;
  * Different character classes (Warrior, Mage, Archer) are concrete products.
  */
 public interface Character {
+    String getName();
+    int getHealth();
+    int getMana();
+    int getStrength();
+    int getIntelligence();
 
-    // TODO: Define common character behaviors
-    // Consider methods like:
-    // - String getName()
-    // - int getHealth()
-    // - int getMana()
-    // - int getStrength()
-    // - int getIntelligence()
-    // - void displayStats()
-    // - void useSpecialAbility()
+    void displayStats();
+    void useSpecialAbility();
 
-    // TODO: Think about equipment
-    // Should characters know about their equipped items?
-    // How will you handle equipping weapons and armor?
-
+    // Методы для экипировки
+    void setWeapon(Weapon weapon);
+    void setArmor(Armor armor);
 }
