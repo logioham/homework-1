f9d8a91a075d1f9811b10307c493138f4f7eb998
logioham <dias.akzhigit@narxoz.kz>
2026-02-13 02:18:09 +0500

    Plate armor class

---
+++ src/main/java/com/narxoz/rpg/equipment/PlateArmor.java
@@ -0,0 +1,18 @@
+package com.narxoz.rpg.equipment;
+
+public class PlateArmor implements Armor {
+    private int defense = 30;
+
+    @Override
+    public int getDefense() { return defense; }
+
+    @Override
+    public String getArmorInfo() {
+        return "Plate Armor (Medieval) - Provides maximum protection.";
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Armor: " + getArmorInfo() + " | Defense: " + defense);
+    }
+}
